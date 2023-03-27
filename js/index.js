02269765312f2bea03b420270a96549e2364054b
Alexander <teichopsia@gmail.com>
2023-03-27 15:00:51 +0000

    practicings..nothing to see here

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("Hi new coyote, let's have some JavaScript fun!")
+const p = document.createElement('p')
+p.innerText = 'Hello World'
+document.body.appendChild(p)
+
+p.style.color = 'red'
+
+const button = document.createElement('button')
+button.innerText = 'Click me'
+document.body.appendChild(button)
+
+const fontSize = 16
+
+button.addEventListener('click', () => {
+  fontSize += 2
+  p.style.fontSize = `${fontSize}px`
+})
